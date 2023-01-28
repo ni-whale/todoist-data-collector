e2f040d9d29d85a0878bb8d1d808aea8d6927565
ni_whale <1nibob1@gmail.com>
2023-01-28 09:36:10 +0200

    Found a way to collect all information for the specific period. The next step is an implementation of the solution by using OOP.

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# from todoist_api_python.api import TodoistAPI
-#
-# api = TodoistAPI("")
-#
-# try:
-#     projects = api.get_projects()
-#     print(projects)
-# except Exception as error:
-#     print(error)
-
 import requests
 import os
 from dotenv import load_dotenv
+import json
 
 
 load_dotenv('//home/ni_whale/Documents/projects/Python/storage.env')
@@ -24,9 +15,14 @@ headers = {'Authorization': f'Bearer {TODOIST_API_TOKEN}'}
 #     'sync_token': "*",
 #     'resource_types': '["completed/get_all "]'
 # }
+# params = {
+#     'since': '2022-12-1T00:00:00',
+#     'until': '2022-12-31T23:59:59',
+#     'limit': 200
+# }
 params = {
     'since': '2022-12-1T00:00:00',
-    'until': '2022-12-31T23:59:59',
+    'until': '2022-12-07T18:48:36.000000Z',
     'limit': 200
 }
 
@@ -35,6 +31,16 @@ try:
     response = requests.get(TODOIST_API_URL, params=params, headers=headers)
     response.raise_for_status()
     todoist_data = response.json()
-    print(todoist_data)
+    # print(todoist_data)
 except Exception as error:
     print(error)
+
+with open('date.json', 'w') as f:
+    json.dump(todoist_data, f, ensure_ascii=False, indent=4)
+
+# last_item = list(todoist_data['items'])[-1::]
+# # print(last_item)
+#
+# for date in last_item:
+#     the_last_date = date['completed_at']
+
